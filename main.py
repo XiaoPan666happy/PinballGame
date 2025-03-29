71af3a2de3988b301c4518295a5a2090bb4c1d9e
XiaoPan <matthew2023@126.com>
2025-03-29 11:28:14 +0800

    HUD帧数

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ pygame.init()
 pygame.font.init()
 
 font_big = pygame.font.SysFont("microsoftyahei", 128, bold=True)
+font_small = pygame.font.SysFont("microsoftyahei", 32, bold=True)
 gameover_surface = font_big.render("GAMEOVER", True, (255, 255, 255))
+
+hud_surface = pygame.surface.Surface((250, 65))
+hud_surface.fill((0, 0, 0))
+hud_surface.set_alpha(128)
 
 screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 clock = pygame.time.Clock()
@@ -59,9 +64,13 @@ while running:
         mouse_pos = pygame.mouse.get_pos()
         board_pos[1] = mouse_pos[1]-50
 
+        fps_text = font_small.render(f"帧数    {round(clock.get_fps(), 2)}", True, (255, 255, 255))
+        
         screen.fill((0, 0, 0))
         pygame.draw.circle(screen, (255, 255, 255), ball_pos, 10)
         pygame.draw.line(screen, (255, 255, 255), board_pos, (board_pos[0], board_pos[1]+100), 4)
+        screen.blit(hud_surface, (0, 0))
+        screen.blit(fps_text, (10, 10))
         pygame.display.flip()
 
         clock.tick(FPS)
