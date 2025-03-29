5b14b947ee07b9a8943a6a7b57c08b0cab5665da
XiaoPan <matthew2023@126.com>
2025-03-29 09:38:31 +0800

    添加弹板

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 clock = pygame.time.Clock()
 ball_pos = [SCREEN_W//2, SCREEN_H//2]
 ball_move_direction = [1, 1]
+board_pos = [100, 100]
 
 running = True
 while running:
@@ -32,12 +33,16 @@ while running:
     # 左侧
     elif ball_pos[0]<0:
         ball_move_direction[0] = -ball_move_direction[0]
+    
+    mouse_pos = pygame.mouse.get_pos()
+    board_pos[1] = mouse_pos[1]-50
 
     screen.fill((0, 0, 0))
     pygame.draw.circle(screen, (255, 255, 255), ball_pos, 10)
+    pygame.draw.line(screen, (255, 255, 255), board_pos, (board_pos[0], board_pos[1]+100), 4)
     pygame.display.flip()
 
     clock.tick(FPS)
-    print(ball_pos)
+    # print(ball_pos)
 
 pygame.quit()
