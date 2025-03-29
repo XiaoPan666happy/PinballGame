6f388bff7bafea57c24c6d406b0467b846625520
XiaoPan <matthew2023@126.com>
2025-03-29 11:04:34 +0800

    添加GAMEOVER

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ count = 0
 ball_pos = [SCREEN_W//2, SCREEN_H//2]
 ball_move_direction = [-1, 1]
 board_pos = [100, 100]
+gameover = False
 
 running = True
 while running:
@@ -20,39 +21,44 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    ball_pos[0] += ball_move_direction[0]*level
-    ball_pos[1] += ball_move_direction[1]*level
-    # 反弹
-    # 下方
-    if ball_pos[1]>SCREEN_H:
-        ball_move_direction[1] = -ball_move_direction[1]
-    # 右侧
-    elif ball_pos[0]>SCREEN_W:
-        ball_move_direction[0] = -ball_move_direction[0]
-    # 上方
-    elif ball_pos[1]<0:
-        ball_move_direction[1] = -ball_move_direction[1]
-    # 左侧
-    elif ball_pos[0]<0:
-        ball_move_direction[0] = -ball_move_direction[0]
-    # 弹板
-    if (board_pos[0]-10 < ball_pos[0] < board_pos[0]+10) and (board_pos[1] < ball_pos[1] < board_pos[1]+100):
-        ball_move_direction[0] = -ball_move_direction[0]
-        if level < 10:
-            count += 1
-            if count >= 32:
-                level += 1
-                count = 0
-    
-    mouse_pos = pygame.mouse.get_pos()
-    board_pos[1] = mouse_pos[1]-50
+    if gameover:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    else:
+        ball_pos[0] += ball_move_direction[0]*level
+        ball_pos[1] += ball_move_direction[1]*level
+        # 反弹
+        # 下方
+        if ball_pos[1]>SCREEN_H:
+            ball_move_direction[1] = -ball_move_direction[1]
+        # 右侧
+        elif ball_pos[0]>SCREEN_W:
+            ball_move_direction[0] = -ball_move_direction[0]
+        # 上方
+        elif ball_pos[1]<0:
+            ball_move_direction[1] = -ball_move_direction[1]
+        # 左侧
+        elif ball_pos[0]<0:
+            ball_move_direction[0] = -ball_move_direction[0]
+            gameover = True
+        # 弹板
+        if (board_pos[0]-10 < ball_pos[0] < board_pos[0]+10) and (board_pos[1] < ball_pos[1] < board_pos[1]+100):
+            ball_move_direction[0] = -ball_move_direction[0]
+            if level < 10:
+                count += 1
+                if count >= 32:
+                    level += 1
+                    count = 0
+        
+        mouse_pos = pygame.mouse.get_pos()
+        board_pos[1] = mouse_pos[1]-50
 
-    screen.fill((0, 0, 0))
-    pygame.draw.circle(screen, (255, 255, 255), ball_pos, 10)
-    pygame.draw.line(screen, (255, 255, 255), board_pos, (board_pos[0], board_pos[1]+100), 4)
-    pygame.display.flip()
+        screen.fill((0, 0, 0))
+        pygame.draw.circle(screen, (255, 255, 255), ball_pos, 10)
+        pygame.draw.line(screen, (255, 255, 255), board_pos, (board_pos[0], board_pos[1]+100), 4)
+        pygame.display.flip()
 
-    clock.tick(FPS)
-    # print(ball_pos)
+        clock.tick(FPS)
+        # print(ball_pos)
 
 pygame.quit()
